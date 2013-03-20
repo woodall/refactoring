f76f5bd4a752592d211309d7e2e4b7a8972f5492
Dave Woodall <dmwoodall@gmail.com>
2013-03-20 11:37:06 -0600

    complete IL class

--- lib/request_pricing_service.rb
+++ lib/request_pricing_service.rb
@@ -1,18 +1,45 @@
 class Illinois
+
   def initialize(request, number_of_pages)
     @request = request
     @number_of_pages = number_of_pages
   end
 
   def price
-    handline_charge = 25.55
-    first_25 = handline_charge + 0.96 * 25
-    first_50 = first_25 + 0.64 * 50
+    return price_above_50_pages if @number_of_pages > 50
+    return price_above_25_pages if @number_of_pages > 25
+    return price_below_25_pages if @number_of_pages > 1
+    return handling_charge
+  end
 
-    return handline_charge if @number_of_pages < 1
-    return (first_50 + (@number_of_pages - 50) * 0.32) if @number_of_pages > 50
-    return (first_25 + (@number_of_pages - 25) * 0.64) if @number_of_pages > 25
-    return (handline_charge + (@number_of_pages) * 0.96)
+private
+
+  HANDLING_CHARGE = 25.55
+  TIER_ONE = 0.96
+  TIER_TWO = 0.64
+
+  def handling_charge
+    HANDLING_CHARGE
+  end
+
+  def setup_costs_for_25_pages
+    handling_charge + TIER_ONE * 25
+  end
+
+  def setup_costs_for_50_pages
+    setup_costs_for_25_pages + TIER_TWO * 50
+  end
+
+  def price_above_50_pages
+    setup_costs_for_50_pages + (@number_of_pages - 50) * 0.32
+  end
+
+  def price_above_25_pages
+    setup_costs_for_25_pages + (@number_of_pages - 25) * 0.64
+  end
+
+  def price_below_25_pages
+    handling_charge + (@number_of_pages) * 0.96
   end
 end
 
